62b7c8c4e039412a5b166fbb6f735496e9fc11ca
PaulV717 <p.vyselko@gmail.com>
2024-03-01 16:26:48 +0200

    [Update] C++ reloading

--- Church_Island/Intermediate/Build/Win64/UnrealEditor/Inc/Church_Island/UHT/Church_Island.init.gen.cpp
+++ Church_Island/Intermediate/Build/Win64/UnrealEditor/Inc/Church_Island/UHT/Church_Island.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeChurch_Island_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xF0CC775B,
-				0x4C8F3F35,
+				0x1DDF6CD0,
+				0xCCC2DA46,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Church_Island.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Church_Island.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Church_Island(Z_Construct_UPackage__Script_Church_Island, TEXT("/Script/Church_Island"), Z_Registration_Info_UPackage__Script_Church_Island, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF0CC775B, 0x4C8F3F35));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Church_Island(Z_Construct_UPackage__Script_Church_Island, TEXT("/Script/Church_Island"), Z_Registration_Info_UPackage__Script_Church_Island, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1DDF6CD0, 0xCCC2DA46));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
